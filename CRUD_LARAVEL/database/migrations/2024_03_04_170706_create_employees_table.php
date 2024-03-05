dda4695307a8d5f7b907711d53246be5432db450
MohammedyasserQabbani <mohammedyasser.qabbani@gmail.com>
2024-03-05 15:03:54 +0300

    Add fields

--- CRUD_LARAVEL/database/migrations/2024_03_04_170706_create_employees_table.php
+++ CRUD_LARAVEL/database/migrations/2024_03_04_170706_create_employees_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("number");
+            $table->string("job_type");
             $table->timestamps();
         });
     }
